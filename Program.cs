7becfd43dd4a9e52c65d0213d6b8efffc0ac5a5a
unknown <sberumen@redlab.com.mx>
2018-09-21 16:56:51 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace triangulo_de_pascal_con_unos_SofiBerumen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lin;
+            
+            Console.WriteLine("Dame base: ");
+            int.TryParse(Console.ReadLine(), out lin);
+
+            int n= lin-1;
+
+            for(int i=1; i<=lin; i++){
+                for(int k=0; k<n; k++){
+                    Console.Write(" ");
+                }
+                
+                for(int j=0; j<i; j++){
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+                n--;
+            }
+        }
+
+        
+    }
+}
